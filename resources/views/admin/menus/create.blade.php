4a1990a9218c49d83a1368878da81224834760ec
Atsushi Hatakeyama <atsushihatakeyama@MacBook-Pro.local>
2022-10-02 10:56:01 +0900

    fixed button display

--- resources/views/admin/menus/create.blade.php
+++ resources/views/admin/menus/create.blade.php
@@ -7,9 +7,8 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex m-2 p-2">
-                <a href="{{ route('admin.menus.index') }}"
-                   class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Menu Index</a>
+            <div class="flex justify-end m-2 p-2">
+                <a href="{{ route('admin.menus.index') }}" class="px-4 py-2 bg-white rounded-lg">Back to Menu</a>
             </div>
             <div class="m-2 p-2 bg-slate-100 rounded">
                 <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
@@ -68,8 +67,7 @@
                             </div>
                         </div>
                         <div class="mt-6 p-4">
-                            <button type="submit"
-                                    class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Store</button>
+                            <button type="submit" class="px-4 py-2 bg-white rounded-lg">Store</button>
                         </div>
                     </form>
                 </div>
--- resources/views/admin/reservations/create.blade.php
+++ resources/views/admin/reservations/create.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-end m-2 p-2">
-                <a href="{{ route('admin.reservations.index') }}" class="px-4 py-2 bg-white rounded-lg">Back to Table</a>
+                <a href="{{ route('admin.reservations.index') }}" class="px-4 py-2 bg-white rounded-lg">Back to Reservation</a>
             </div>
 
             <div class="m-2 p-2 bg-slate-500 rounded">
