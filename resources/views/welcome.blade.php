58f4386e67a9920c5979f4292c44db39601c077b
Atsushi Hatakeyama <atsushihatakeyama@MacBook-Pro.local>
2022-10-06 23:55:09 +0900

    added flontend design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,11 +12,11 @@
             culpa excepturi quae commodi quaerat,
         </div>
         <div class="flex flex-col items-center mt-12 text-center">
-            <span class="relative inline-flex w-full md:w-auto">
-{{--                <a href="{{ route('reservations.step.one') }}" type="button"--}}
-{{--                   class="inline-flex items-center justify-center px-6 py-2 text-base font-bold leading-6 text-white bg-green-600 rounded-full lg:w-full md:w-auto hover:bg-green-500 focus:outline-none">--}}
-{{--                    Make your Reservation--}}
-{{--                </a>--}}
+            <span class="relative inline-flex w-full md:w-auto"></span>
+                <a href="{{ route('reservations.step.one') }}" type="button"
+                   class="inline-flex items-center justify-center px-6 py-2 text-base font-bold leading-6 text-white bg-green-600 rounded-full lg:w-full md:w-auto hover:bg-green-500 focus:outline-none">
+                    Make your Reservation
+                </a>
         </div>
     </div>
     <!-- End Main Hero Content -->
@@ -25,13 +25,9 @@
             <div class="flex flex-wrap items-center sm:-mx-3">
                 <div class="w-full md:w-1/2 md:px-3">
                     <div class="w-full pb-6 space-y-4 sm:max-w-md lg:max-w-lg lg:space-y-4 lg:pr-0 md:pb-0">
-                        <!-- <h1
-              class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl"
-            > -->
-                        <h3 class="text-xl">OUR STORY
-                        </h3>
+                        <h1 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl"></h1>
+                        <h3 class="text-xl">OUR STORY</h3>
                         <h2 class="text-4xl text-green-600">Welcome</h2>
-                        <!-- </h1> -->
                         <p class="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus nemo incidunt
                             praesentium, ipsum
