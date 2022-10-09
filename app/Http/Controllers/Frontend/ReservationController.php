4ad16a8c741ac836223f721a3e4dd88cd7e7ce70
Atsushi <atu729@icloud.com>
2022-10-09 12:02:28 +0900

    added reservation step one

--- app/Http/Controllers/Frontend/ReservationController.php
+++ app/Http/Controllers/Frontend/ReservationController.php
@@ -3,12 +3,43 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Reservation;
+use App\Rules\DateBetween;
+use App\Rules\TimeBetween;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    public function index()
+    public function stepOne(Request $request)
     {
-        return 'step one';
+        $reservation = $request->session()->get('reservation');
+        $min_date = Carbon::today();
+        $max_date = Carbon::now()->addWeek();
+        return view('reservations.step-one', compact('reservation', 'min_date', 'max_date'));
+    }
+
+    public function storeStepOne(Request $request)
+    {
+        $validated = $request->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            'res_date' => ['required', 'date', new DateBetween, new TimeBetween],
+            'tel_number' => ['required'],
+            'guest_number' => ['required'],
+        ]);
+
+        if (empty($request->session()->get('reservation'))) {
+            $reservation = new Reservation();
+            $reservation->fill($validated);
+            $request->session()->put('reservation', $reservation);
+        } else {
+            $reservation = $request->session()->get('reservation');
+            $reservation->fill($validated);
+            $request->session()->put('reservation', $reservation);
+        }
+
+        return to_route('reservations.step.two');
     }
 }
